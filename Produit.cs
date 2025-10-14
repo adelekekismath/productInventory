2491a1dafb949130bfe58c9dc4827bae0a8b0240
adelekekismath <adelekekismath9@gmail.com>
2025-10-14 08:41:07 +0200

    Add initial project files and structure including .gitignore, Produit class, and solution file

---
+++ Produit.cs
@@ -0,0 +1,33 @@
+namespace ProducInventory
+{
+    class Produit {
+        public string Nom { get; set; }
+        public string Reference { get; set; }
+        public double Prix { get; set; }
+        public int Quantite { get; set; }
+        public string Categorie { get; set; }
+
+        public Produit(string nom, string reference, double prix, int quantite, string categorie)
+        {
+            Nom = nom;
+            Reference = reference;
+            Prix = prix;
+            Quantite = quantite;
+            Categorie = categorie;
+        }
+
+        public override string ToString()
+        {
+            return $"Référence du produit : {Reference}\n" +
+                   $"Nom du produit : {Nom}\n" +
+                   $"Prix du produit : {Prix}\n" +
+                   $"Quantité du produit : {Quantite}\n" +
+                   $"Catégorie du produit : {Categorie}";
+        }
+        
+        public double ValeurStock()
+        {
+            return Prix * Quantite;
+        }
+    }
+}
